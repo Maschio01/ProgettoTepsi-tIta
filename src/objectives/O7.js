99b897d299fa20304896157e7c333ea7a46d8946
Simone Panini <paninisimone@istitutocorni.it>
2021-04-29 23:08:49 +0200

    dsf

--- src/objectives/O7.js
+++ src/objectives/O7.js
@@ -1,6 +1,57 @@
 function objective(){
     return (
+        <div id="content">
+            <div id="sideMenu">
+                <p> </p>
+            </div>
+            <div>
+                <div id="contentTitle">
+                    <h1>7 - Energia pulita e accessibile</h1>
+                </div>
+                <div id="contentText">
+                    <div className="banner" style={{backgroundColor:"#fbc412"}}>
+                        <img src="../img/problemIcon7.jpg" alt="banner"/>
+                    </div>
+                    <p>L’energia è un elemento centrale per quasi tutte le sfide e le opportunità più importanti che il mondo si trova oggi ad affrontare. Che sia per lavoro, sicurezza, cambiamento climatico, produzione alimentare o aumento dei redditi,
+                        l’accesso all’energia è essenziale.<br/><br/>Il Segretario Generale ONU Ban Ki-moon è stato iniziatore dell’iniziativa Energia Rinnovabile per Tutti (Sustainable Energy for All) per assicurare l’accesso universale ai servizi energetici
+                        moderni, migliorare l’efficienza energetica e accrescere l’uso di risorse rinnovabili.
 
+                    </p>
+                    <div>
+                        <img src="../img/ob7/Agenda-2030-goal-7-investimenti-energia-pulita.png" alt="data" style={{width: "70%", height: "400px"}}/>
+                        <figcaption>Il grafico mosra l'avanzamento dell'uso di energia pulita</figcaption>
+                    </div>
+                    <h3>-Fatti e Cifre:</h3>
+                    <table>
+                        <tr>
+                            <td>persone che non hanno accesso a moderni mezzi elettrici</td>
+                            <td>1 persona su 5</td>
+                        </tr>
+                        <tr>
+                            <td>persone che dipendono da legno, carbone, carbonella o concime animale per cucinare e per scaldarsi</td>
+                            <td>3 miliardi</td>
+                        </tr>
+
+
+                    </table>
+                    <h3>-Traguardi:</h3>
+                    <ul>
+                        <li>1) Garantire entro il 2030 accesso a servizi energetici che siano convenienti, affidabili e moderni.</li>
+                        <li>2) Aumentare considerevolmente entro il 2030 la quota di energie rinnovabili nel consumo totale di energia.</li>
+                        <li>3) Raddoppiare entro il 2030 il tasso globale di miglioramento dell’efficienza energetica.</li>
+                        <li>a) Accrescere entro il 2030 la cooperazione internazionale per facilitare l’accesso alla ricerca e alle tecnologie legate all’energia pulita.</li>
+                        <li>b) Implementare entro il 2030 le infrastrutture e migliorare le tecnologie per fornire servizi energetici moderni e sostenibili, specialmente nei paesi meno sviluppati.</li>
+                    </ul>
+                    <h3>-Esportazione energia nel mondo</h3>
+                    <p>Per esportare l'energia nel mondo è necessario sfruttare le fonti che quel paese ha da offrire, come acqua, sole e vento. E finanzire tali operazioni con l'intento di migliorare e rendere ecquo l'accesso all'energia a livello mondiale.
+                    </p>
+                    <img src="../img/ob7/NAZIONALI_ASVIS2020_GOAL07.jpg" alt="data" style={{width: "35%", height: "400px"}}/>
+                    <figcaption>grafico sull'uso dell'energia pulita</figcaption>
+                </div>
+            </div>
+            <div style={{clear: "both"}}></div>
+
+        </div>
     );
 }
 
